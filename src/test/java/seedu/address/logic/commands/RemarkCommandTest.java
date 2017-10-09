ab8018258ef8a9de6279eca4373ece02df190f41
Denny Sunarjo <sunarjo.denny@gmail.com>
2017-10-09 10:49:09 +0800

    Remove trailing white spaces, add newline at eof, reorder imports

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.Test;
 
 import seedu.address.logic.CommandHistory;
@@ -8,10 +12,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.RemarkCommand.MESSAGE_NOT_IMPLEMENTED_YET;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 /**
  * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
  */
@@ -19,7 +19,7 @@ import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 public class RemarkCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-    
+
     @Test
     public void execute() throws Exception {
         assertCommandFailure(prepareCommand(), model, MESSAGE_NOT_IMPLEMENTED_YET);
